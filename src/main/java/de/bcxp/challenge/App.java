a7d08109f77ac5ed226f1ed54cca0b0302688bab
7h0m4sS4 <48961543+7h0m4sS4@users.noreply.github.com>
2024-02-02 14:53:15 +0100

    updated main application to solve the challenge

--- src/main/java/de/bcxp/challenge/App.java
+++ src/main/java/de/bcxp/challenge/App.java
@@ -1,5 +1,11 @@
 package de.bcxp.challenge;
 
+import de.bcxp.challenge.data.CountrieData;
+import de.bcxp.challenge.data.WeatherData;
+import de.bcxp.challenge.io.DataLoader;
+import de.bcxp.challenge.io.DataLoaderFactory;
+import de.bcxp.challenge.io.FileConfiguration;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -12,12 +18,34 @@ public final class App {
      */
     public static void main(String... args) {
 
-        // Your preparation code …
+        //Create a new instance of the DataLoader
+        DataLoader dataLoader = DataLoaderFactory.getDataLoader(FileConfiguration.WEATHER_CSV);
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        System.out.printf("Day with smallest temperature spread: %s%n", dayWithSmallestTempSpread);
+        //Create a new instance of the WeatherData
+        if (dataLoader != null) {
+            WeatherData weatherData = null;
+            try {
+                weatherData = dataLoader.loadWeatherFromSource(FileConfiguration.WEATHER_CSV);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            String dayWithSmallestTempSpread = weatherData.calculateDayWtihMinTempSpread();
+            System.out.printf("Day with smallest temperature spread: %s%n", dayWithSmallestTempSpread);
+        }
 
-        String countryWithHighestPopulationDensity = "Some country"; // Your population density analysis function call …
-        System.out.printf("Country with highest population density: %s%n", countryWithHighestPopulationDensity);
+        //Create a new instance of the DataLoader
+        dataLoader = DataLoaderFactory.getDataLoader(FileConfiguration.COUNTRIES_CSV);
+
+        //Create a new instance of the CountrieData
+        if (dataLoader != null) {
+            CountrieData countrieData = null;
+            try {
+                countrieData = dataLoader.loadCountryFromSource(FileConfiguration.COUNTRIES_CSV);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            String countryWithHighestPopulationDensity = countrieData.calculateCountryWithHighestPopulationDensity();
+            System.out.printf("Country with highest population density: %s%n", countryWithHighestPopulationDensity);
+        }
     }
 }
